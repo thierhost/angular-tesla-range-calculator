4a6f286f448cb90b3886dc8fda6ca48e04e21604
John Papa <john@johnpapa.net>
2017-01-14 16:44:59 -0500

    tutorial complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'tesla-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  logo: string = 'assets/logo.svg';
 }
